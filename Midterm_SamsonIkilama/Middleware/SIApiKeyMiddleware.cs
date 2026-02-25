2260ceb47edce86dc7fb7fb26fd86f131951f3a3
Samson Ikilama <mayindiikilama@gmail.com>
2026-02-25 18:12:15 -0500

    Implemented global APiKey middleware

--- Midterm_SamsonIkilama/Middleware/SIApiKeyMiddleware.cs
+++ Midterm_SamsonIkilama/Middleware/SIApiKeyMiddleware.cs
@@ -2,6 +2,39 @@
 {
     public class SIApiKeyMiddleware
     {
-        private readonly
+        private readonly RequestDelegate _next;
+        private const string API_KEY = "MIDTERM_KEY_123_SI";
+
+        public SIApiKeyMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            if (!context.Request.Headers.TryGetValue("X-Api-Key", out var extractedKey))
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsJsonAsync(new
+                {
+                    error = "Unauthorized",
+                    message = "Invalid or missing API key."
+                });
+                return;
+            }
+
+            if (extractedKey != API_KEY)
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsJsonAsync(new
+                {
+                    error = "Unauthorized",
+                    message = "Invalid or missing API key."
+                });
+                return;
+            }
+
+            await _next(context);
+        }
     }
 }
