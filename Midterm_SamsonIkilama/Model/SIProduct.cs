ff241f6972c620a969919726c4b937d92e4cd696
Samson Ikilama <mayindiikilama@gmail.com>
2026-02-25 17:14:58 -0500

    Updated  SIProduct with addition of properties and validations

--- Midterm_SamsonIkilama/Model/SIProduct.cs
+++ Midterm_SamsonIkilama/Model/SIProduct.cs
@@ -1,6 +1,20 @@
-﻿namespace Midterm_SamsonIkilama.Model
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Midterm_SamsonIkilama.Model
 {
     public class SIProduct
     {
+        [Required]
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(25)]
+        public string Name { get; set; } = null!;
+
+
+        [Required]
+        [StringLength(25)]
+        public int Quantity { get; set; }
     }
 }
