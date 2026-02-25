1387c1f6d6536476eb9b694cd149a14b6597793c
Samson Ikilama <mayindiikilama@gmail.com>
2026-02-25 17:18:33 -0500

    addition of SIProductController

---
+++ Midterm_SamsonIkilama/Controllers/SIProductController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Midterm_SamsonIkilama.Controllers
+{
+    public class SIProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
