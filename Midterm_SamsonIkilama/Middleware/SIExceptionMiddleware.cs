854e56934caf136b1d95c9f22987843fc7a39e01
Samson Ikilama <mayindiikilama@gmail.com>
2026-02-25 18:07:48 -0500

    Craeted exception Middleware

---
+++ Midterm_SamsonIkilama/Middleware/SIExceptionMiddleware.cs
@@ -0,0 +1,29 @@
+﻿namespace Midterm_SamsonIkilama.Middleware
+{
+    public class SIExceptionMiddleware
+    {
+        private readonly RequestDelegate _next;
+        public SIExceptionMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                context.Response.StatusCode = 500;
+                context.Response.ContentType = "application/json";
+                var errorResponse = new
+                {
+                    error = "InternalServerError",
+                    message = ex.Message
+                };
+                await context.Response.WriteAsJsonAsync(errorResponse);
+            }
+        }
+    }
+}
